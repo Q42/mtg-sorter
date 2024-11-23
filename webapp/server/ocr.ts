d328d43ce3b56347fe16954282ff8a2ffaa511be
Lukas van Driel <lukas@q42.nl>
2024-11-23 14:48:28 +0100

    updated vertex model

--- webapp/server/ocr.ts
+++ webapp/server/ocr.ts
@@ -1,6 +1,11 @@
-import { VertexAI } from "@google-cloud/vertexai";
+import {
+  HarmBlockThreshold,
+  HarmCategory,
+  VertexAI,
+} from "@google-cloud/vertexai";
 import axios from "axios";
 import sharp from "sharp";
+import fs from "fs";
 
 export async function whatCardIsInFrontOfTheCamera(ip: string = "10.71.16.46") {
   const body = (
@@ -29,7 +34,7 @@ async function ocrImage(fileBase64: string) {
     project: "q42puzzles-wordlists",
     location: "europe-west1",
   });
-  const model = "gemini-1.5-flash";
+  const model = "gemini-1.5-flash-002";
 
   // Instantiate the models
   const generativeModel = vertex_ai.preview.getGenerativeModel({
@@ -39,6 +44,24 @@ async function ocrImage(fileBase64: string) {
       temperature: 1,
       topP: 1,
     },
+    safetySettings: [
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+      },
+    ],
   });
   const filePart = {
     inline_data: { data: fileBase64, mimeType: "image/jpeg" },
@@ -51,6 +74,11 @@ async function ocrImage(fileBase64: string) {
     contents: [{ role: "user", parts: [textPart, filePart] }],
   };
   const response = await generativeModel.generateContent(req as any);
+  console.log(JSON.stringify(response, null, 2));
+  fs.writeFileSync(
+    "./server/data/response.json",
+    JSON.stringify(response, null, 2)
+  );
   const text = response.response.candidates?.[0].content.parts?.[0].text ?? "";
 
   function getstring(input: string) {
